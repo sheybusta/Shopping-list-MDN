6534352fbb0071fdbd101afb648141c8211d8d82
Sheyla <shel.busta@gmail.com>
2021-07-28 11:37:03 +0200

    sort done

--- src/components/sortList.js
+++ src/components/sortList.js
@@ -1,18 +1,33 @@
 import { handleModalHide } from "../handlers/handleModalHide.js";
 
+const sort = [ 
+'Unmark items', 'a-z', 'z-a'
+]
+
 export const  sortList = () => {
     const div = document.createElement("div");
     div.classList = "modal-content";
-    const titlee = document.createElement("h1"); // delete in future
-    titlee.innerText = "hello!"; // delete future
-    div.appendChild(titlee); // delete future
     const button = document.createElement("button");
     button.addEventListener("click", handleModalHide);
     button.classList = "close";
     button.innerText = "close";
     div.appendChild(button);
+    // create ul 
+    // loop to get li from sort 
+    const ul = document.createElement("ul");
+    div.appendChild(ul);
+
+
+    for (let i = 0; i<sort.length; i++) {
+        const li = document.createElement("li");    
+        li.setAttribute("value", sort[i]);
+        li.innerText = sort[i];
+
+        ul.appendChild(li);
+    }
+
     return div;
-};
+}; 
 
     // data.forEach (item => { //data is parameter => state.items
     //     const itemEl = document.createElement("li");
